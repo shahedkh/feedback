de5e7a85484978e5dc302df5ec46e6df86722dbc
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-21 01:14:29 +0300

    add data and feedbacl list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
 function App() {
+  const [feedback , setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header/>
+      <div className="container">
+      <FeedbackList feedback={feedback} />
+      </div>
     </>
   );
 }
