d678de3a03f13e7d1593bf93e95c4d17d584b83b
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-21 05:27:47 +0300

    add rating

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Button from "./shared/Button";
 import Card from "./shared/Card";
+import RatingSelect from './RatingSelect'
 
-const FeedbackForm = () => {
+const FeedbackForm = ({handleAdd}) => {
     const [text , setText] = useState('')
     const [btnDisabled , setBtnDisabled] = useState(true)
     const [message , setMessage] = useState('')
+    const [rating , setRating] = useState(10)
 
     const handleTextChange = (e) => {
       if( text === ""){
@@ -19,19 +21,32 @@ const FeedbackForm = () => {
         setMessage(null)
       }
       
-      
-      
         setText(e.target.value)
+    }
+ 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating 
+            }
+            handleAdd(newFeedback)
+
+            setText('')
+        }
     }
 
   return (
     <Card>
+        <form onSubmit={handleSubmit}>
       <h2>How would you rate your service with us?</h2>
+      <RatingSelect />
       <div className="input-group">
         <input onChange={handleTextChange} value={text} type='text' placeholder='write a review'/>
         <Button type="submit" isDisabled={btnDisabled}>Send</Button>
       </div>
-      {message && <div className="message">{message}</div>}
+      {message && <div className="message">{message}</div>}</form>
     </Card>
   );
 };
