41a88d5ecfe00e76dc8e6e9bdc3fe05cfd0b0070
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-21 05:05:49 +0300

    add validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,23 @@ import Card from "./shared/Card";
 
 const FeedbackForm = () => {
     const [text , setText] = useState('')
+    const [btnDisabled , setBtnDisabled] = useState(true)
+    const [message , setMessage] = useState('')
 
     const handleTextChange = (e) => {
+      if( text === ""){
+        setBtnDisabled(true)
+        setMessage(null)
+      }else if (text !== '' && text.trim().length <=10 ){
+        setBtnDisabled(true)
+        setMessage("Text must be at least 10 characters")
+      }else{
+        setBtnDisabled(false)
+        setMessage(null)
+      }
+      
+      
+      
         setText(e.target.value)
     }
 
@@ -14,8 +29,9 @@ const FeedbackForm = () => {
       <h2>How would you rate your service with us?</h2>
       <div className="input-group">
         <input onChange={handleTextChange} value={text} type='text' placeholder='write a review'/>
-        <Button type="submit" >Send</Button>
+        <Button type="submit" isDisabled={btnDisabled}>Send</Button>
       </div>
+      {message && <div className="message">{message}</div>}
     </Card>
   );
 };
