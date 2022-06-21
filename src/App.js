e0bcbf420427231944a16687cd8f090e657541b9
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-21 03:32:46 +0300

    add feedbacl stats and delete icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+
 function App() {
-  const [feedback , setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are You sure ?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
-      <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,16 @@
-const FeedbackItem = ({item}) => {
+import Card from "./shared/Card";
+import {FaTimes} from 'react-icons/fa'
+
+const FeedbackItem = ({item , handleDelete}) => {
+
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() =>handleDelete(item.id)}>
+        <FaTimes color="purple"/>
+      </button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 };
 
