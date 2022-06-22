afe97de4e250d4e8f7d8a633196186f0a713aff4
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-22 05:20:04 +0300

    add context

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -23,14 +24,14 @@ setFeedback([newFeedback,...feedback])
 
 
   return (
-    <>
+    <FeedbackProvider>
       <Header />
       <div className="container">
         <FeedbackForm handleAdd={addFeedback}/>
         <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
-    </>
+    </FeedbackProvider>
   );
 }
 
