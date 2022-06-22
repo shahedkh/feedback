804d8c7b8b502d8ce8774d5bc2262ab86c6cfc72
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-22 15:02:08 +0300

    edit feedback event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,29 +1,52 @@
-import { createContext , useState } from "react";
-import {v4 as uuidv4} from 'uuid'
+import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
+const FeedbackContext = createContext();
 
-const FeedbackContext = createContext()
-
-export const FeedbackProvider = ({children}) => {
-const [feedback , setFeedback] = useState([
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
     {
-        id : 1 ,
-        text : 'This item is from context',
-        rating: 10
-    }
-])
+      id: 1,
+      text: "This item is from context",
+      rating: 10,
+    },
+  ]);
 
-const addFeedback = (newFeedback) => {
-  newFeedback.id = uuidv4()
-  setFeedback([newFeedback,...feedback])
-    }
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
-const deleteFeedback = (id) => {
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
     if (window.confirm("Are You sure ?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
-    return <FeedbackContext.Provider value={{ feedback , deleteFeedback,addFeedback }}>{children}</FeedbackContext.Provider>
-}
-export default FeedbackContext
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+export default FeedbackContext;
