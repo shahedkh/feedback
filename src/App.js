c4e0449f378fa4c5a15909c68db5f423f0a04398
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-22 14:38:53 +0300

    moving functions to context

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import {v4 as uuidv4} from 'uuid'
-import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-newFeedback.id = uuidv4()
-setFeedback([newFeedback,...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are You sure ?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
-
-
   return (
     <FeedbackProvider>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackForm />
         <FeedbackStats />
-        <FeedbackList handleDelete={deleteFeedback} />
+        <FeedbackList  />
       </div>
     </FeedbackProvider>
   );
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,6 @@
 import { createContext , useState } from "react";
+import {v4 as uuidv4} from 'uuid'
+
 
 const FeedbackContext = createContext()
 
@@ -11,9 +13,17 @@ const [feedback , setFeedback] = useState([
     }
 ])
 
+const addFeedback = (newFeedback) => {
+  newFeedback.id = uuidv4()
+  setFeedback([newFeedback,...feedback])
+    }
 
+const deleteFeedback = (id) => {
+    if (window.confirm("Are You sure ?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
-
-    return <FeedbackContext.Provider value={{ feedback }}>{children}</FeedbackContext.Provider>
+    return <FeedbackContext.Provider value={{ feedback , deleteFeedback,addFeedback }}>{children}</FeedbackContext.Provider>
 }
 export default FeedbackContext
